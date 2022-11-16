81a7e253b06a9e372be8350691afa60bec9e435d
Yoga Vignesh MK <94665513+Yogesh2210@users.noreply.github.com>
2022-11-16 10:01:33 +0530

    Delete Project Development Phase/Sprint 2 directory

--- Project Development Phase/Sprint 2/Sprint-02/CCR/Static/Js/pass.js
+++
@@ -1,9 +0,0 @@
-function showPassword(){
-    var x = document.getElementById("password-input");
-
-    x.type = x.type == "password" ? "text" : "password";
-
-    var x = document.getElementById("password-input1");
-
-    x.type = x.type == "password" ? "text" : "password";
-}
